161ab33843e0b33842afffb93f9378ecb40499e3
zmv <zmv@email.com>
2022-03-17 00:46:21 +0400

    completing location

--- src/js/app.js
+++ src/js/app.js
@@ -16,3 +16,21 @@ hideAndToggle()
 
 import { initSpoilers } from "./modules/spoiler.js"
 document.addEventListener("DOMContentLoaded", initSpoilers)
+
+const mapFrame = document.querySelector(".location__map iframe")
+function resizeMap(e) {
+   if (mapFrame) {
+      if (document.documentElement.offsetWidth <= 320) {
+         console.log("<= 320")
+         if (mapFrame.height !== "200") mapFrame.height = "200"
+      } else if (document.documentElement.offsetWidth <= 480) {
+         console.log("<= 480")
+         if (mapFrame.height !== "300") mapFrame.height = "300"
+      } else {
+         console.log("else")
+         if (mapFrame.height !== "400") mapFrame.height = "400"
+      }
+   }
+}
+window.addEventListener("resize", resizeMap)
+document.addEventListener("DOMContentLoaded", resizeMap)
