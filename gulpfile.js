a3023bd2e6e45e68735ae66daed9b2be8e9e9dec
zmv <zmv@mail.com>
2023-03-29 17:58:38 +0400

    updating gulp/ * gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,20 @@ global.app = {
    plugins: plugins,
    isBuild: process.argv.includes("--build"),
    isDev: !process.argv.includes("--build"),
+   isBrowsersync: process.argv.includes("--browsersync"),
+   isWebpfy: process.argv.includes("--webpfy"),
+   isImagemin: process.argv.includes("--imagemin"),
+   isGzip: process.argv.includes("--gzip"),
 }
 import { copy } from "./gulp/tasks/copy.js"
-import { reset } from "./gulp/tasks/reset.js"
+import { resetEverything, resetPathChildren } from "./gulp/tasks/reset.js"
 import { html } from "./gulp/tasks/html.js"
 import { scss } from "./gulp/tasks/scss.js"
 import { js } from "./gulp/tasks/js.js"
 import { images } from "./gulp/tasks/images.js"
-import { otf2ttf, ttf2woff, fontsStyle } from "./gulp/tasks/fonts.js"
+import { video } from "./gulp/tasks/video.js"
+import { data } from "./gulp/tasks/data.js"
+import { copyOtf, otf2ttf, ttf2woff, fontsStyle, copyTtf, copyFonts } from "./gulp/tasks/fonts.js"
 import { svgSprite } from "./gulp/tasks/svgSprite.js"
 import { zip } from "./gulp/tasks/zip.js"
 import { ftp } from "./gulp/tasks/ftp.js"
@@ -28,18 +34,44 @@ function watcher() {
    gulp.watch(path.watch.scss, scss)
    gulp.watch(path.watch.js, js)
    gulp.watch(path.watch.images, images)
+   gulp.watch(path.watch.video, video)
+   gulp.watch(path.watch.data, data)
 }
 export { svgSprite }
 
-const fonts = gulp.series(otf2ttf, ttf2woff, fontsStyle)
-const mainTasks = gulp.series(
-   fonts,
-   gulp.parallel(copy, html, scss, js, images)
-)
-const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server))
-const build = gulp.series(reset, mainTasks)
-const deployZip = gulp.series(reset, mainTasks, zip)
-const deployFtp = gulp.series(reset, mainTasks, ftp)
+// some plugins may require to have css ready before processing html
+// this applies to the plugins that exract essential css to html
+const processHtmlAfterScss = false
 
-export { dev, build, fontsStyle, deployZip, deployFtp }
+const devFonts = copyFonts
+const buildFonts = gulp.series(copyOtf, otf2ttf, copyTtf, ttf2woff, fontsStyle)
+const fonts = app.isBuild ? buildFonts : devFonts
+const scssAndHtmlInParallel = gulp.series(
+   fonts,
+   gulp.parallel(copy, html, scss, js, images, video, data)
+)
+const scssAfterHtml = gulp.series(fonts, scss, html, gulp.parallel(copy, js, images, video, data))
+const mainTasks = processHtmlAfterScss ? scssAfterHtml : scssAndHtmlInParallel
+const dev = gulp.series(resetEverything, mainTasks, gulp.parallel(watcher, server))
+const compile = gulp.series(resetEverything, scssAndHtmlInParallel)
+const update = gulp.series(scssAndHtmlInParallel)
+const build = gulp.series(resetEverything, scssAndHtmlInParallel)
+const deployZip = gulp.series(resetEverything, scssAndHtmlInParallel, zip)
+const deployFtp = gulp.series(resetEverything, scssAndHtmlInParallel, ftp)
+
+gulp.task("reset", resetEverything)
+
+export { dev, build, buildFonts, copyOtf, otf2ttf, fontsStyle, deployZip, deployFtp, fonts }
 gulp.task("default", dev)
+gulp.task("compile", compile)
+gulp.task("update", update)
+gulp.task("images", images)
+
+gulp.task("scss", async function () {
+   // console.log(`${app.path.build.css}*`)
+   scss()
+})
+
+import { testWebpfy } from "./gulp/tasks/testWebpfy.js"
+gulp.task("test", gulp.series(testWebpfy))
+gulp.task("js", js)
