8513bcc60a8f9db1932a322ecf346ec27f96fbb4
zmv <zmv@mail.com>
2023-03-29 18:02:41 +0400

    making swiper stadalone

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,10 @@
 import * as functions from "./modules/functions.js"
-import { initSliders } from "./swiper.js"
 
 functions.isWebp()
 
 import { DynamicAdapt } from "./modules/dynamic-adapt.js"
 const da = new DynamicAdapt("max")
 da.init()
-
-document.addEventListener("DOMContentLoaded", function () {
-   initSliders()
-})
 
 import { hideAndToggle } from "./modules/hider-and-toggler.js"
 hideAndToggle()
