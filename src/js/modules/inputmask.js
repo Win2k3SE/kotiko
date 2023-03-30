e5d8e31267dd4d84c2e7f8d9102cd277681fcbb2
zmv <zmv@mail.com>
2023-03-30 21:26:10 +0400

    making inputmask work on several fields

--- src/js/modules/inputmask.js
+++ src/js/modules/inputmask.js
@@ -1,23 +1,25 @@
 import Inputmask from "inputmask"
 
 export function mask() {
-   const mask = Inputmask.default({
+   const maskOptions = {
       mask: "+9 (999) 999-99-99",
       clearMaskOnLostFocus: true,
       showMaskOnHover: false,
-      onfocus: {},
-   })
+   }
 
-   const phone = document.querySelector("[name=phone]")
-   phone.addEventListener("focusin", (e) => {
-      if (e.target.value === "") {
-         mask.mask("[name=phone]")
-      }
-   })
-   phone.addEventListener("focusout", (e) => {
-      if (e.target.value === "") {
-         phone.placeholder = "Телефон"
-         mask.remove()
-      }
-   })
+   const phones = document.querySelectorAll("[name=phone]")
+   for (const phone of phones) {
+      const mask = Inputmask.default(maskOptions)
+      phone.addEventListener("focusin", (e) => {
+         if (e.target.value === "") {
+            mask.mask(e.target)
+         }
+      })
+      phone.addEventListener("focusout", (e) => {
+         if (e.target.value === "") {
+            e.target.placeholder = "Телефон"
+            mask.remove()
+         }
+      })
+   }
 }
