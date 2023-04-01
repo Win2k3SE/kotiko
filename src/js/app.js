9af060fcd65cdcd9d6f1d4c5e818a0f490da2941
zmv <zmv@mail.com>
2023-04-01 21:11:59 +0400

    removing location resizer

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ burger()
 import { mask } from "./modules/inputmask.js"
 mask()
 
-import {isWebp, debounce } from "./modules/functions.js"
+import {isWebp } from "./modules/functions.js"
 
 isWebp()
 
@@ -20,24 +20,6 @@ hideAndToggle()
 
 import { initSpoilers } from "./modules/spoiler.js"
 document.addEventListener("DOMContentLoaded", initSpoilers)
-
-const mapFrame = document.querySelector(".location__map iframe")
-function resizeMap(e) {
-   if (mapFrame) {
-      if (document.documentElement.offsetWidth <= 320) {
-         // console.log("<= 320")
-         if (mapFrame.height !== "200") mapFrame.height = "200"
-      } else if (document.documentElement.offsetWidth <= 480) {
-         // console.log("<= 480")
-         if (mapFrame.height !== "300") mapFrame.height = "300"
-      } else {
-         // console.log("else")
-         if (mapFrame.height !== "400") mapFrame.height = "400"
-      }
-   }
-}
-window.addEventListener("resize", debounce(resizeMap))
-document.addEventListener("DOMContentLoaded", resizeMap)
 
 import { createCustomSelectEl } from "./modules/dynamic-select.js"
 document.querySelectorAll(".dynamic-select").forEach((el) => createCustomSelectEl(el))
