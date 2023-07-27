54d822924ecb1945c41e400d3325f2a68ba3d469
zmv <zmv@mail.com>
2023-07-27 15:39:07 +0400

    move Choices init from index to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -15,6 +15,23 @@ da.init()
 import { hideAndToggle } from "./modules/hider-and-toggler.js"
 hideAndToggle()
 
+const selector = '.order [name="suite-type"]'
+const els = document.querySelectorAll(selector)
+for(const el of els) {
+    new LazyLoad({
+        elements_selector: selector,
+        unobserve_enered: true,
+        callback_enter: () =>{
+            new Choices(el, {
+            searchEnabled: false,
+            itemSelectText: "",
+            shouldSort: false,
+            allowHTML: false,
+            })
+        }
+    })
+}
+
 import { initSpoilers } from "./modules/spoiler.js"
 document.addEventListener("DOMContentLoaded", initSpoilers)
 
