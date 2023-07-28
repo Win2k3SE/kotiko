eb51f61acdaa47607f62e916244101fa3062d87a
zmv <zmv@mail.com>
2023-07-29 01:16:10 +0400

    rename --css-map -> --cssmap

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ global.app = {
    isCssmin: process.argv.includes("--cssmin"),
    isGzip: process.argv.includes("--gzip"),
    isPrefixCss: process.argv.includes("--prefix-css"),
-   isCssMap: process.argv.includes("--css-map"),
+   isCssMap: process.argv.includes("--cssmap"),
    isKeepUncompressedCss: process.argv.includes("--keep-uncompressed-css"),
    isVersionNumber: process.argv.includes("--version-number"),
    isBuildFonts: process.argv.includes("--build-fonts"),
