84b5b3be4ad856211e2155358c98515fa68d950d
zmv <zmv@mail.com>
2023-08-02 00:02:38 +0400

    no longer lazy loading choices

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import {isWebp } from "./modules/functions.js"
+import { isWebp } from "./modules/functions.js"
 isWebp()
 
 import { loadScript } from "./modules/functions.js";
@@ -6,13 +6,12 @@ import { loadScript } from "./modules/functions.js";
 new LazyLoad({
     elements_selector: ".order-form__form",
     callback_enter: () => {
-        console.log('loading datepicker!')
         loadScript('js/datepicker.min.js')
     }
 })
 
 
-import {burger} from './modules/burger.js'
+import { burger } from './modules/burger.js'
 burger()
 
 import { mask } from "./modules/inputmask.js"
@@ -25,26 +24,6 @@ da.init()
 import { hideAndToggle } from "./modules/hider-and-toggler.js"
 hideAndToggle()
 
-function initChoices() {
-    const selector = '.order [name="suite-type"]'
-    const els = document.querySelectorAll(selector)
-    for(const el of els) {
-        new LazyLoad({
-            elements_selector: selector,
-            unobserve_enered: true,
-            callback_enter: () => {
-                    new Choices(el, {
-                    searchEnabled: false,
-                    itemSelectText: "",
-                    shouldSort: false,
-                    allowHTML: false,
-                })
-            }
-        })
-    }
-}
-initChoices()
-
 import { initSpoilers } from "./modules/spoiler.js"
 document.addEventListener("DOMContentLoaded", initSpoilers)
 
@@ -54,14 +33,14 @@ togglePlaceholderOnFocus()
 import { validate } from "./modules/form-validation.js"
 validate()
 
-for(const el of document.querySelectorAll('[data-clipboard]')) {
-   el.addEventListener('click', () => {
-      navigator.clipboard.writeText(el.getAttribute('data-clipboard'))
-      el.classList.add('copy-success')
-      setTimeout(() => {
-         el.classList.remove('copy-success')
-      }, 400);
-   })
+for (const el of document.querySelectorAll('[data-clipboard]')) {
+    el.addEventListener('click', () => {
+        navigator.clipboard.writeText(el.getAttribute('data-clipboard'))
+        el.classList.add('copy-success')
+        setTimeout(() => {
+            el.classList.remove('copy-success')
+        }, 400);
+    })
 }
 
 import { orderPopup } from './modules/order-popup.js'
